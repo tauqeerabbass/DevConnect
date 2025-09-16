6ca20634190b92b733dd51bc881e3d7d23cdb118
tauqeerabbas <tauqeerabbas334@gmail.com>
2025-09-17 02:08:02 +0500

    Implemented feed API to show recommendations to user.

--- src/routes/user.js
+++ src/routes/user.js
@@ -2,6 +2,7 @@ const express = require("express");
 const userRouter = express.Router();
 const { authUser } = require("../middlewares/auth");
 const connectionRequest = require("../models/connectionRequest");
+const User = require("../models/user");
 
 const USER_SAFE_DATA = "firstName lastName age gender bio";
 
@@ -45,6 +46,43 @@ userRouter.get("/user/connections", authUser, async (req, res) => {
 
     } catch (error) {
         res.status(500).send("Error fetching connections: " + error.message);
+    }
+});
+
+userRouter.get("/feed", authUser, async (req, res) => {
+    try {
+        const loggedInUser = req.user;
+
+        const page = parseInt(req.query.pagee) || 1;
+        let limit = parseInt(req.query.limit) || 10;
+        const skip = (page-1)*limit;
+        
+        limit = limit>50 ? 50 : limit;
+
+        const connectionRequests = await connectionRequest.find({
+            $or :[
+                {fromUserId: loggedInUser._id},
+                {toUserId: loggedInUser._id}
+            ]
+        }).select("fromUserId toUserId");
+
+        const excludeUserIds = new Set();
+        connectionRequests.forEach((req) => {
+            excludeUserIds.add(req.fromUserId.toString());
+            excludeUserIds.add(req.toUserId.toString());
+        });
+
+        const users = await User.find({
+            $and : [
+                {_id: {$nin: Array.from(excludeUserIds)}},
+                {_id: {$ne: loggedInUser._id}}
+            ]
+        }).select(USER_SAFE_DATA).skip(skip).limit(limit);
+
+        res.json({ message: "User feed", data: users });
+
+    } catch (error) {
+        res.status(500).send("Error fetching feed: " + error.message);
     }
 });
 
